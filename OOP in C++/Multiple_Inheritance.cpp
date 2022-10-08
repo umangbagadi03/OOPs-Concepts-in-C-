bc8045f297740016ae630b3e7be2591d50ebcbf9
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2022-10-08 22:31:39 +0530

    Add files via upload

---
+++ OOP in C++/Multiple_Inheritance.cpp
@@ -0,0 +1,35 @@
+/* 
+A class can also be derived from more than one base class, using a comma-separated list:
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "Some content in parent class." ;
+    }
+};
+
+// Another base class
+class MyOtherClass {
+  public:
+    void myOtherFunction() {
+      cout << "Some content in another class." ;
+    }
+};
+
+// Derived class
+class MyChildClass: public MyClass, public MyOtherClass {
+};
+
+int main() {
+  MyChildClass myObj;
+  myObj.myFunction();
+  myObj.myOtherFunction();
+  return 0;
+}
+
+
